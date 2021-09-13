73ad0c5ca6a04cf16b6aae897d68d2f4bf7de918
Matthew J <Jone6552@bears.unco.edu>
2021-09-13 15:14:16 -0600

    Week2 Superheroes

--- week2/Superhero/hero/views.py
+++ week2/Superhero/hero/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class DrStrangeView(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Dr. Strange',
+            'body': 'My name is Stephen Strange',
+            'image': '/static/images/strange.jpg'
+        }
+
+
+class DeadpoolView(TemplateView):
+    template_name = "hero.html"
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Deadpool',
+            'body': 'My name is Wade Wilson',
+            'image': '/static/images/deadpool.jpg'
+        }
+
+
+class SpidermanView(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Spiderman',
+            'body': 'My name is Peter Parker',
+            'image': '/static/images/spiderman.jpg'
+        }
